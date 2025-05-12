de23ffabb75c841d3756738f99b732033a840747
Thorstone <Niklasgth@hotmail.com>
2025-05-12 13:48:55 +0200

    setup

---
+++ timepoker-backend/src/test/java/com/timepoker_backend/timepoker_backend/TimepokerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timepoker_backend.timepoker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimepokerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
