2cb7f31a18effea6030139ef8cc2764af32720d0
AiwendilCiunio <ciunio@gmail.com>
2025-05-17 01:08:48 +0200

    Implemented password hashing. Implemented login and register functions with validation. Tweaked endpoints in UserController to not expose passwords.

---
+++ src/main/java/com/timepoker_backend/timepoker_backend/DTO/UserResponseDTO.java
@@ -0,0 +1,11 @@
+package com.timepoker_backend.timepoker_backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserResponseDTO {
+    private String id;
+    private String userName;
+}
