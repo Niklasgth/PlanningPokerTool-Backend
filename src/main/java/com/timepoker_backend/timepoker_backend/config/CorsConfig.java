a9e3969ee9dc53b741c9a0968906bbcaf7a02873
nin3ty9 <user.email>
2025-05-19 16:50:41 +0200

    deploy fix cors 2

--- src/main/java/com/timepoker_backend/timepoker_backend/config/CorsConfig.java
+++ src/main/java/com/timepoker_backend/timepoker_backend/config/CorsConfig.java
@@ -8,8 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${ALLOWED_ORIGINS}")
+    @Value("${allowed.origins}")
     private String allowedOrigins;
+
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
